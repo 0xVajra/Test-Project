c7efd7beb6277468071c4daa82e524995bab95ff
Rookie1337 <112756969+Rookie1337@users.noreply.github.com>
2023-11-06 02:30:36 +0300

    Add2 +
    Navbar refactor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,37 @@
 import { Layout, Menu, Row } from "antd"
 import { FC } from "react"
 import { useTypedSelector } from "../hooks/useTypedSelector"
+import { useNavigate } from "react-router-dom"
+import { RouteNames } from "../router"
+import { useAction } from "../hooks/useAction"
 
 
 const Navbar: FC = () => {
-    const {isAuth} = useTypedSelector(state => state.auth)
+    const router = useNavigate()
+    const {isAuth, user} = useTypedSelector(state => state.auth)
+    const {logout} = useAction()
    return (
     <Layout.Header>
         <Row justify="end">
             {isAuth
             ?
             <Menu theme="dark" mode="horizontal" selectable={false}>
-                <div style={{color:"white"}}>Ochir</div>
-                <Menu.Item key={1}>Выйти</Menu.Item>
+                <div style={{color:"white"}}>{user.username}</div>
+                <Menu.Item 
+                    onClick={logout}
+                    key={1}
+                >
+                    Выйти
+                </Menu.Item>
            </Menu>
             :
             <Menu theme="dark" mode="horizontal" selectable={false}>
-                <Menu.Item key={1}>Логин</Menu.Item>
+                <Menu.Item onClick={() => router(RouteNames.LOGIN)} key={1}>Логин</Menu.Item>
            </Menu>
             }
-           
         </Row>
     </Layout.Header>
-   )
-
-
-}
+   );
+};
 
 export default Navbar;
